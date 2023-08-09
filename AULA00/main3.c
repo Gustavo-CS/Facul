8c919fe2e8de67c94c47d8178bded93170b0a71a
Gustavo Carvalho Santos <65356496+Gustavo-CS@users.noreply.github.com>
2023-08-09 11:25:03 -0300

    Add files via upload

--- AULA00/main3.c
+++ AULA00/main3.c
@@ -2,7 +2,7 @@
 int main(){
     int salario;
     int ano;
-    salario = 200000;
+    salario = 2000000;
     ano = 2024;
-    printf("Desenvolvedor full stack: \nSalário: %i, \nAno: %i", salario, ano);
+    printf("Desenvolvedor full stack: \nSalário: U$%i, \nAno: %i", salario, ano);
 }
