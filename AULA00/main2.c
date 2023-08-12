9c6839ad83cf3b5c5b88da25b5cfd789ff5ba6b1
Gustavo Carvalho Santos <65356496+Gustavo-CS@users.noreply.github.com>
2023-08-12 11:23:18 -0300

    Delete AULA00 directory

--- AULA00/main2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int peso;
-    int idade;
-    peso = 2000;
-    idade = 18;
-    printf("%i\n%i", peso, idade);
-    // printf("%i", idade);
-}
--- AULA00/main3.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int salario;
-    int ano;
-    salario = 2000000;
-    ano = 2024;
-    printf("Desenvolvedor full stack: \nSalário: U$%i, \nAno: %i", salario, ano);
-}
