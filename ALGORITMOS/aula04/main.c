3524de97644e9c50f959b3924c0d9e84bff2570e
Gustavo-CS <gustavocarvalho9340@gmail.com>
2023-09-11 17:52:13 -0300

    aula

--- ALGORITMOS/aula04/main.c
+++ ALGORITMOS/aula04/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("%.2f", 2);
+    int hora;
+    int minutos;
+    int resultado;
+    printf("que horas são (só as horas): ");
+    scanf("%d", &hora);
+    printf("quantos minutos são (só os minutos): ");
+    scanf("%d", &minutos);
+    resultado = (hora * 60) + minutos;
+    printf("ja se passaram %.d minutos desde o inicio do dia", resultado);
 }
