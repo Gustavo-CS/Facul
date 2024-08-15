27a64a6ea5dfe3367126f0e2003bb93492b93f50
GUSTAVO CARVALHO SANTOS <gustavo.santos2023@a.ucb.br>
2024-08-15 11:28:17 -0300

    aula 2 poo verdadeira

---
+++ ProgramacaoOrientadaOjetos/Aula2/src/br/com/visao/ObjetoAprovado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.visao;
+
+import br.com.controle.Aprovado;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author gustavo.santos2023
+ */
+public class ObjetoAprovado {
+
+    public static void main(String[] args) {
+        Aprovado a = new Aprovado();
+        a.fodase();
+    }
+}
