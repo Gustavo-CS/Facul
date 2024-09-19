96477ed510fd1482fde8c846ee1b9685998e988c
GUSTAVO CARVALHO SANTOS <gustavo.santos2023@a.ucb.br>
2024-09-19 11:12:35 -0300

    aula 6 polimorfismo

---
+++ ProgramacaoOrientadaOjetos/Polimorfismo/src/br/com/controle/Subtracao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+/**
+ *
+ * @author gustavo.santos2023
+ */
+public class Subtracao extends OperacaoMatematica{
+    public Double calcular(Double valor1, Double valor2){
+        return valor1-valor2;
+    }
+}
