7836b531e2772ead44bea15596c6d2174eff6e2d
Gustavo Carvalho Santos <65356496+Gustavo-CS@users.noreply.github.com>
2025-03-06 09:11:28 -0300

    Create Th.py

---
+++ novas-tecnologias/atividades/Th.py
@@ -0,0 +1,19 @@
+# Recursive Python function to solve the tower of hanoi
+mov=0
+
+def TowerOfHanoi(n , source, destination, auxiliary):
+    global mov
+    if n==1:
+        print ("Move disk 1 from source",source,"to destination",destination)
+        mov+=1
+        return
+    TowerOfHanoi(n-1, source, auxiliary, destination)
+    print ("Move disk",n,"from source",source,"to destination",destination)
+    mov+=1
+    TowerOfHanoi(n-1, auxiliary, destination, source)
+        
+# Driver code
+n = 5
+TowerOfHanoi(n,'A','B','C') 
+print("num mov ", mov)
+# A, C, B are the name of rods
