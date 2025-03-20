1936d8545765b1813c9c6e3b99c05b7a2e16f94e
GUSTAVO CARVALHO SANTOS <gustavo.santos2023@a.ucb.br>
2025-03-20 09:24:49 -0300

    atividade 3 prog web

--- prog-web/atividade03/script.js
+++ prog-web/atividade03/script.js
@@ -20,25 +20,83 @@ botaoDe.onclick = function () {
 var caracterNumber = 0;
 var caracterCount = document.getElementById("caracterCount");
 var nText = document.getElementById("nText");
-console.log(nText);
-nText.addEventListener("keypress", function(event){
-    
-    if(event.key !== " ") {
-        caracterNumber++;
-        document.getElementById("caracterCount").textContent = "numero de caracteres: " + caracterNumber;
-    }
-    if(event.key === "Enter") {
+
+var newDiv = document.createElement("div");
+nText.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
         // event.preventDefault();
         caracterNumber = 0;
         document.getElementById("caracterCount").textContent = "numero de caracteres: " + caracterNumber;
-        const newDiv = document.createElement("div");
-        const newContent =document.createTextNode(nText.value);
-        newDiv.appendChild(newContent);
-        const currentDiv = document.getElementById("caracterCount");
+        const newContent = document.createTextNode(nText.value);
+        const intDiv = document.createElement("div");
+        intDiv.appendChild(newContent);
+        newDiv.appendChild(intDiv);
+        const currentDiv = document.getElementById("final");
         document.body.insertBefore(newDiv, currentDiv);
         nText.value = "";
     }
+    else if (event.key === "Backspace") {
+        if (caracterNumber > 0) {
+            nText = document.getElementById("nText");
+            if (nText.value[nText.value.length - 1] !== " ") {
+                caracterNumber--;
+                document.getElementById("caracterCount").textContent = "numero de caracteres: " + caracterNumber;
+            }
+        }
+    }
+    else if (event.key !== " ") {
+        caracterNumber++;
+        document.getElementById("caracterCount").textContent = "numero de caracteres: " + caracterNumber;
+    }
 })
+
+var botaoRe = document.getElementById("botaoRe");
+botaoRe.onclick = function () {
+    // newDiv.remove();
+    newDiv.remove();
+    newDiv = document.createElement("div");
+    numDiv.remove();
+    numDiv = document.createElement("div");
+    lista = []
+    numClick = 0;
+    document.getElementById("contador").textContent = "Contador: " + numClick;
+    caracterNumber = 0;
+    document.getElementById("caracterCount").textContent = "numero de caracteres: " + caracterNumber;
+    nText.value = "";
+}
+
+var lista = [];
+
+var numDiv = document.createElement("div");
+
+var botaoList = document.getElementById("botaoList");
+botaoList.onclick = function () {
+    var num = Math.floor(Math.random() * 10);
+    lista.push(num);
+    const numContent = document.createTextNode(num);
+    const intDivNum = document.createElement("div");
+    intDivNum.appendChild(numContent);
+    numDiv.appendChild(intDivNum);
+    const currentDiv = document.getElementById("final2");
+    document.body.insertBefore(numDiv, currentDiv);
+}
+
+var botaoOr = document.getElementById("botaoOr");
+botaoOr.onclick = function () {
+    lista.sort();
+    numDiv.remove();
+    numDiv = document.createElement("div");
+    for (let i = 0; i < lista.length; i++) {
+        const numContent = document.createTextNode(lista[i]);
+        const intDivNum = document.createElement("div");
+        intDivNum.appendChild(numContent);
+        numDiv.appendChild(intDivNum);
+        const currentDiv = document.getElementById("final2");
+        document.body.insertBefore(numDiv, currentDiv);
+    }
+}
+
+
 
 // console.log(getElementById("body"));
 // nText.ke = function() {
